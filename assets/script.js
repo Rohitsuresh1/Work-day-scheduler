d8c0c91c9dd327a7e93174d5753584c2ea1aa04f
Rohitsuresh1 <99383088+Rohitsuresh1@users.noreply.github.com>
2022-05-20 13:27:49 -0400

    added localstorage

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,22 @@
 var date= moment().format("dddd, MMMM Do, YYYY ");
 $("#currentDay").text(date).addClass("font-weight-bolder");
 
+var color = function(i) {
+    var hour= new Date().getHours();
+    console.log(hour);
+    while(i>=0){
+        console.log("number",i);
+        i--;
+        
+
+    }
+};
+
+var getData = function(i){
+    var data=localStorage.getItem(i);
+    $(`input#taskText${i}`).val(data);
+};
+
 var times = function(i){
     var ampm = "";
     if(i<=2){
@@ -17,12 +33,19 @@ var times = function(i){
 for ( var i=0; i<9; i++){
     var row= $("<div class='row'>");
     var columnTime=$(`<div class='col-sm-1 bg-light time border-top border-secondary'>${times(i)}</div>`);
-    var columnDisc=$(`<div class='col-sm-9 bg-secondary'></div>`);
-    var columnSave=$(`<div class='col-sm-1 bg-success save rounded'><buttom id="saveBtn${i} class="savebtn "><i class="fas fa-save"> Save</i></button></div>`);
+    var columnDisc=$(`<div class='col-sm-9 '><input id="taskText${i}" class="text" type="text" name="input"></div>`);
+    var columnSave=$(`<div class='col-sm-1 rounded'><button data-id="${i}" type="button" class="btn btn-success"><i class="fas fa-save"> Save</i></button></div>`);
     row.append(columnTime);
     row.append(columnDisc);
     row.append(columnSave);
     $(".container").append(row);
-    
+    getData(i);    
 };
+
+$("button.btn.btn-success").click(function(e){
+    var id=$(this).data("id");
+    var data=$(this).parent().siblings().find("input").val();
+    localStorage.setItem(id,data);
+    color(id);
+});
 
